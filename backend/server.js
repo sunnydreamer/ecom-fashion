43bd511e77ba9e227b4a2db8c7585f5cc4db5199
sunnydreamer <sunnylee9516@gmail.com>
2023-03-20 00:20:23 -0400

    env update

--- backend/server.js
+++ backend/server.js
@@ -19,13 +19,10 @@ dotenv.config();
 // require("./config/database");
 
 const DB = mongoose
-  .connect(
-    process.env.DATABASE.replace("<password>", process.env.DATABASE_PASSWORD),
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    }
-  )
+  .connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => {
     console.log("DB CONNECTION SUCCESSFUL!");
   });
