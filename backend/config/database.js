6e7182f854446232960dd6e955aa22f780e16bf9
sunnydreamer <sunnylee9516@gmail.com>
2023-03-19 23:09:40 -0400

    database update

--- backend/config/database.js
+++ backend/config/database.js
@@ -2,7 +2,11 @@ const mongoose = require("mongoose");
 
 const DB = mongoose
   .connect(
-    process.env.DATABASE.replace("<password>", process.env.DATABASE_PASSWORD)
+    process.env.DATABASE.replace("<password>", process.env.DATABASE_PASSWORD),
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
   )
   .then(() => {
     console.log("DB CONNECTION SUCCESSFUL!");
